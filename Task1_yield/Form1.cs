4d4479474438d6b2231fcf52500a1b12a079174e
Archangel <djarchangel3737@gmail.com>
2017-11-08 21:54:16 +0300

    code styling and small refactoring

--- Task1_yield/Form1.cs
+++ Task1_yield/Form1.cs
@@ -1,92 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task1_yield
 {
     public partial class Form1 : Form
     {
+        private DateTime _a, _b;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        DateTime a,b;
-        
         public void to_Date_Time()
         {
             //пытаемся конвертнуть текст из текстбоксов в переменные DateTime
-            try 
+            try
             {
-                a = Convert.ToDateTime(textBoxA.Text);
-                b = Convert.ToDateTime(textBoxB.Text);
+                _a = Convert.ToDateTime(textBoxA.Text);
+                _b = Convert.ToDateTime(textBoxB.Text);
             }
-            //другого эксепшена не смог добиться, было предположение перегрузить (ArgumentOutOfRangeException),
+            //другого эксепшена не смог добиться, было предположение перегрузить(ArgumentOutOfRangeException),
             //но не получилось придумать кейс - год с 0001 работает, 9999 тоже
-                //FIXED - более корректная и полная по инфе обработка исключения
-            catch (FormatException e)  
+            //FIXED - более корректная и полная по инфе обработка исключения
+            catch (FormatException e)
             {
-                string ErrorMessage = e.Message + Environment.NewLine + Environment.NewLine                    
-                    + e.StackTrace + Environment.NewLine + Environment.NewLine 
-                    + e.Source + Environment.NewLine
-                    + e.Data;
-                MessageBox.Show(ErrorMessage, "Error", MessageBoxButtons.OK);
+                var errorMessage = e.Message + Environment.NewLine + Environment.NewLine
+                                   + e.StackTrace + Environment.NewLine + Environment.NewLine
+                                   + e.Source + Environment.NewLine
+                                   + e.Data;
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK);
             }
-            
-
         }
 
         private void button_Evaluate_Click(object sender, EventArgs e)
         {
             richTextBox_Result.Text = "";
-            
+
             to_Date_Time();
             //if (Range(a, b).Count() >=1) //слишком тяжёлая операция на большой дистанции, а .First() крэшит при отсутствии элемента, пока не вижу иного способа
             //FIXED
-            if (Range(a,b).FirstOrDefault() != default(DateTime)) //Прозрел на решение, проблема проверки на непустоту первого значения решена
-            {
-                foreach (DateTime Dta in Range(a, b))
-                {
-                    richTextBox_Result.Text += Dta.ToString("D", new System.Globalization.CultureInfo("en-US")) + Environment.NewLine;
-                }
-            }
+            if (Range(_a, _b).FirstOrDefault() != default(DateTime)
+            ) //Прозрел на решение, проблема проверки на непустоту первого значения решена
+                foreach (var dta in Range(_a, _b))
+                    richTextBox_Result.Text += dta.ToString("D", new CultureInfo("en-US")) + Environment.NewLine;
             else
-                richTextBox_Result.Text = "There is no dates between";
+                richTextBox_Result.Text = "There are no dates between";
         }
 
         //требуемый метод, можно и сокращённо т.к. в using есть 
         //IEnumerable<DateTime> Range(DateTime first, DateTime second)
         //внутри требуемые yield
-        public static System.Collections.Generic.IEnumerable<DateTime> Range(DateTime first, DateTime second)
+        public static IEnumerable<DateTime> Range(DateTime first, DateTime second)
         {
-            TimeSpan Difference; 
+            TimeSpan Difference;
             DateTime result;
 
             if (second > first.AddDays(1))
             {
                 Difference = second - first;
-                for (int i = 1; i < Difference.Days; i++)
+                for (var i = 1; i < Difference.Days; i++)
                 {
                     result = first.AddDays(i);
                     yield return result;
                 }
-
             }
             else
             {
                 MessageBox.Show("First date must be less, than second", "Error", MessageBoxButtons.OK);
-                
-                yield break;
             }
-           
         }
-
-
     }
 }
