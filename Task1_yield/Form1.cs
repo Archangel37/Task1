774c10522de4c3894cdcd44617b47e1374b77217
Archangel37 <djarchangel3737@gmail.com>
2017-11-23 09:55:27 +0300

    fixed conditions, refactoring

--- Task1_yield/Form1.cs
+++ Task1_yield/Form1.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Task1_yield
@@ -18,21 +16,25 @@ namespace Task1_yield
 
         public void to_Date_Time()
         {
-            //пытаемся конвертнуть текст из текстбоксов в переменные DateTime
+            //пытаемся конвертнуть текст из текстбоксов в переменные DateTime и сделать с ними положенные действия =)
             try
             {
                 _startDate = Convert.ToDateTime(textBoxA.Text);
                 _endDate = Convert.ToDateTime(textBoxB.Text);
+
+                //Fixed: Move this part of code from button - because only if we have succeeded converting to Dates, we can do something with them
+                var resultText = string.Empty;
+                //FIXED: -if (Range(_startDate, _endDate).FirstOrDefault() != default(DateTime)); -else
+                foreach (var dta in Range(_startDate, _endDate))
+                    //FIXED -> -CultureInfo.InvariantCulture
+                    resultText += dta.ToString("D") + Environment.NewLine;
+                if (string.IsNullOrEmpty(resultText))
+                    resultText = "There are no dates between";
+                richTextBox_Result.Text = resultText;
             }
-            //FIXED - более корректная и полная по инфе обработка исключения
             catch (FormatException e)
             {
                 var errorMessage = e.Message;
-                // оставил для себя комментом для дальнейшего изучения, что ещё можно снимать с эксепшенов
-                // + Environment.NewLine + Environment.NewLine
-                //                   + e.StackTrace + Environment.NewLine + Environment.NewLine
-                //                   + e.Source + Environment.NewLine
-                //                   + e.Data;
                 //FIXED: +MessageBoxIcon.Error
                 MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
@@ -40,39 +42,24 @@ namespace Task1_yield
 
         private void button_Evaluate_Click(object sender, EventArgs e)
         {
-            //Fixed my poor code style -> -"" / +String.Empty
             richTextBox_Result.Text = string.Empty;
-            //FIXED: +resultText
-            var resultText = string.Empty;
             to_Date_Time();
-            //FIXED before
-            if (Range(_startDate, _endDate).FirstOrDefault() != default(DateTime))
-                foreach (var dta in Range(_startDate, _endDate))
-                    //FIXED -> -new System.Globalization.CultureInfo("en-US") ) / +CultureInfo.InvariantCulture
-                    resultText += dta.ToString("D", CultureInfo.InvariantCulture) + Environment.NewLine;
-            else
-                resultText = "There are no dates between";
-
-            richTextBox_Result.Text = resultText;
         }
 
         //внутри требуемый yield
         //FIXED: +startDate, endDate
+        //FIXED: inverting conditions - if => argEx, for => yield return
         public static IEnumerable<DateTime> Range(DateTime startDate, DateTime endDate)
         {
-            if (endDate > startDate.AddDays(1))
-            {
-                //FIXED: cycle 'for'
-                for (var d = startDate.AddDays(1); d < endDate; d = d.AddDays(1))
-                    yield return d;
-            }
-            else
+            if (endDate <= startDate.AddDays(1))
             {
                 //FIXED: +argEx
                 var argEx = new ArgumentException("Start date must be less, than end");
                 MessageBox.Show(argEx.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 throw argEx;
             }
+            for (var d = startDate.AddDays(1); d < endDate; d = d.AddDays(1))
+                yield return d;
         }
     }
 }
